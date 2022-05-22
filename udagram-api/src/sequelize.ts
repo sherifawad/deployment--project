31c77c830d8af5501b9c0c4e2fb5d47c160a31a7
Sherif Awad <eng.sherifawad@gmail.com>
2022-05-22 14:15:45 +0200

    change env values

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -6,9 +6,8 @@ export const sequelize = new Sequelize({
 	password: config.password,
 	database: config.database,
 	host: config.host,
-	port: config.port,
+	port: config.db_port,
 
 	dialect: "postgres",
 	storage: ":memory:",
 });
-
